8f3826c32f10272ca1b218088e4215840ea25080
Jaskaranbir Singh <jaskaranbir_singh@ymail.com>
2015-07-26 16:07:41 +0530

    Delete old fixes script

--- fixes.sh
+++
@@ -1,6 +0,0 @@
-#This script auto applies the required fixes to final zip
-#Please run only after fullota
-#========================================================
-
-cd fixes
-zip -g -r ../out/fullota.zip META-INF
